9f9a7cb84af72ee569353ac6a2367059a746c128
Wandering-Sayo <sayo_hidaka@hotmail.com>
2017-10-13 20:04:07 -0300

    while for arrays

--- Program.cs
+++ Program.cs
@@ -15,20 +15,45 @@ namespace Primero
             
             Console.WriteLine("What's poppin?");
             Console.WriteLine("Mi nombre es " + primer_nombre + "-" + nombre + ". Tengo " + edad + " años." );
+           
+           
             Console.WriteLine("Cual es tu nombre?");
             nombre = Console.ReadLine();
-            Console.WriteLine("Bienvendo " + nombre);
-            Console.WriteLine("Cuál es tu edad?");
-            edad = int.Parse(Console.ReadLine());
+           
+           while(!nombre.Equals("salir")) //para comparar Strings se usa siempre Equals y no ==
+           {
+                //mientras se cumple la condición entra
+                Console.WriteLine("Bienvendo " + nombre);
+                Console.WriteLine("Cuál es tu edad?");
+                edad = int.Parse(Console.ReadLine());
 
-            if(edad >= 18)
-            {
-                Console.WriteLine("Sos mayor de edad.");
-            }
-            else
-            {
-                Console.WriteLine("Sos menor de edad.");
-            }
+                if(edad >= 18)
+                {
+                    Console.WriteLine("Sos mayor de edad.");
+                }
+                else
+                {
+                    Console.WriteLine("Sos menor de edad.");
+                }
+
+                 Console.WriteLine("Cual es tu nombre?");
+                 nombre = Console.ReadLine();
+           }
+           
+           for(int i=0;i<5;i++)
+           {
+               Console.WriteLine("Hola");
+           } 
+
+           /* Se podría hacer tambien con while: 
+           int j=0;
+           while(j<5)
+           {
+               Console.WriteLine("Hola");
+               j++;
+           }
+            */
+
 
             //Switch
 
@@ -44,6 +69,32 @@ namespace Primero
                 default:
                     Console.WriteLine("Tiene " + args.Length + " parámetros");
                     break;
+            }
+
+
+            //Array
+            string color = "rojo";
+            string[] colores = new string []{ "verde" , "blanco" , color , "violeta" };
+            Console.WriteLine("El tercer color es el: " + colores[2]);
+            colores[0] = "negro"; //cambia el item en posición 0
+
+            for(int k=0; k < colores.Length ; k++)
+            {
+                Console.WriteLine("Color: " + colores[k]);
+            }
+
+            foreach(string item in colores)
+            {
+                Console.WriteLine("Color: " + item);
+            }
+
+            int [,] multi = {{1,3,4}, {2,4,2}, {1,3,4}};
+            for(int l=0; l< 3; l++ )
+            {
+                for(int m=0; m< 3; m++ )
+                {
+                    Console.WriteLine(multi[l,m]);
+                }
             }
 
 
